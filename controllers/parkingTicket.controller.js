eef6760ea10412e0c464cc3128b929eca464e675
hiteshdotcom <hiteshpal.8097@gmail.com>
2024-07-13 00:40:43 +0530

    minor bug resolve

--- controllers/parkingTicket.controller.js
+++ controllers/parkingTicket.controller.js
@@ -500,7 +500,7 @@ export const getAllTickets = async (req, res) => {
   try {
     const { search = "" } = req.query;
     const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.page) || 10;
+    const limit = Number(req.query.limit) || 10;
     let query = {};
     if (search) {
       query.$or = [
