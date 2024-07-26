d6276a86ea6f390ec9c423a988995e1b251a796d
Aditya Singh <emails4adi@gmail.com>
2024-07-26 21:44:19 +0530

    Updating super code to capital letters

--- utils/helperFunctions.js
+++ utils/helperFunctions.js
@@ -21,7 +21,7 @@ export const isEmpty = (value) => {
 export const generateCode = (length = 6) => {
   let result = "";
   const characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
   const charactersLength = characters.length;
   for (let i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
