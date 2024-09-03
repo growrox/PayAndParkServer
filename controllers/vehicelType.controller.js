e0256a71d11d2a06eea19a69ace56beaf453363d
Aditya Singh <emails4adi@gmail.com>
2024-09-03 21:38:17 +0530

    vehical gst model added

--- controllers/vehicelType.controller.js
+++ controllers/vehicelType.controller.js
@@ -1,14 +1,14 @@
 import VehicleType from "../models/vehicleType.model.js"; // Assuming your model file is in 'models/user.model.js'
 import path from "path";
-import { __dirname } from "../utils/helperFunctions.js";
+import { __dirname, isEmpty } from "../utils/helperFunctions.js";
 import fs from "fs";
 import { getLanguage } from "../utils/helperFunctions.js";
 import { responses } from "../utils/Translate/vehicalType.response.js";
 
 export const createVehicleType = async (req, res) => {
-  const language = getLanguage(req,responses);
+  const language = getLanguage(req, responses);
   try {
-    const { name } = req.body;
+    const { name, gstPercentage } = req.body;
     const hourlyPrices = JSON.parse(req.body.hourlyPrices);
     const imageUrl = req.file
       ? `/images/${req.params.folderName}/${req.file.filename}`
@@ -16,47 +16,58 @@ export const createVehicleType = async (req, res) => {
 
     const newVehicleType = new VehicleType({
       name,
+      gstPercentage,
       image: imageUrl,
       hourlyPrices,
     });
+
+    if (isEmpty(gstPercentage) || +gstPercentage < 0) {
+      return res.status(202).json({ error: "GST percentage is required." });
+    }
+
     await newVehicleType.save();
     return res.status(201).json({
-      message: responses.messages[language ].vehicleTypeCreatedSuccessfully,
+      message: responses.messages[language].vehicleTypeCreatedSuccessfully,
       result: newVehicleType
     });
   } catch (error) {
-    return res.status(500).json({ error: responses.errors[language ].internalServerError });
+    console.error({ error });
+    return res.status(500).json({ error: responses.errors[language].internalServerError });
   }
 };
 
 export const getAllVehicleType = async (req, res) => {
-  const language = getLanguage(req,responses);
+  const language = getLanguage(req, responses);
 
   try {
     const vehicleTypes = await VehicleType.find({});
+
     const vehicleTypesWithImageUrl = vehicleTypes.map((vehicleType) => ({
       ...vehicleType._doc,
+      hourlyPrices: vehicleType._doc.hourlyPrices.map(el => {
+        return { ...el._doc, price: el.price + Math.round(+el.price * (vehicleType.gstPercentage / 100)) };
+      }),
       image: `${req.protocol}://${req.get("host")}/api/v1/${vehicleType.image}`,
     }));
 
     return res.status(200).json({
-      message: responses.messages[language ].allVehicleTypesList,
+      message: responses.messages[language].allVehicleTypesList,
       result: vehicleTypesWithImageUrl
     });
   } catch (error) {
-    return res.status(500).json({ error: responses.errors[language ].internalServerError });
+    return res.status(500).json({ error: responses.errors[language].internalServerError });
   }
 };
 
 export const serveImage = (req, res) => {
-  const language = getLanguage(req,responses);
+  const language = getLanguage(req, responses);
 
   try {
     const { imageName, folderName } = req.params;
     const imagePath = path.join(__dirname, "../images/", folderName, imageName);
     return res.sendFile(imagePath, (err) => {
       if (err) {
-        return res.status(404).json({ error: responses.errors[language ].imageNotFound });
+        return res.status(404).json({ error: responses.errors[language].imageNotFound });
       }
     });
   } catch (error) {
@@ -65,35 +76,46 @@ export const serveImage = (req, res) => {
 };
 
 export const getVehicleTypeDetail = async (req, res) => {
-  const language = getLanguage(req,responses);
+  const language = getLanguage(req, responses);
 
   try {
-    const vehicleType = await VehicleType.findById(req.params.id);
+
+    let vehicleType = await VehicleType.findById(req.params.id);
     if (!vehicleType) {
-      return res.status(404).json({ error: responses.errors[language ].vehicleTypeNotFound });
+      return res.status(404).json({ error: responses.errors[language].vehicleTypeNotFound });
     }
 
     vehicleType.image = `${req.protocol}://${req.get("host")}/api/v1/${vehicleType.image}`;
+
+    const myvehicalnewPrice = vehicleType.hourlyPrices.map(el => {
+
+      return {
+        ...el._doc,
+        includingGst: el.price + Math.round(+el.price * (vehicleType.gstPercentage / 100)),
+      };
+    })
+
     return res.status(200).json({
-      message: responses.messages[language ].vehicleTypeDetail,
-      result: vehicleType
+      message: responses.messages[language].vehicleTypeDetail,
+      result: { ...vehicleType._doc, hourlyPrices: myvehicalnewPrice }
     });
   } catch (error) {
-    return res.status(500).json({ error: responses.errors[language ].internalServerError });
+    console.log("Error ", error)
+    return res.status(500).json({ error: responses.errors[language].internalServerError });
   }
 };
 
 export const updateVehicleType = async (req, res) => {
-  const language = getLanguage(req,responses);
+  const language = getLanguage(req, responses);
 
   try {
-    const { name } = req.body;
+    const { name, gstPercentage } = req.body;
     const hourlyPrices = JSON.parse(req.body.hourlyPrices);
     const newImage = req.file ? `images/vehicle-type/${req.file.filename}` : null;
 
     const vehicleType = await VehicleType.findById(req.params.id);
     if (!vehicleType) {
-      return res.status(404).json({ error: responses.errors[language ].vehicleTypeNotFound });
+      return res.status(404).json({ error: responses.errors[language].vehicleTypeNotFound });
     }
 
     if (newImage && vehicleType.image) {
@@ -106,6 +128,9 @@ export const updateVehicleType = async (req, res) => {
     }
 
     vehicleType.name = name;
+    if (!isEmpty(gstPercentage)) {
+      vehicleType.gstPercentage = gstPercentage;
+    }
     vehicleType.hourlyPrices = hourlyPrices;
     if (newImage) {
       vehicleType.image = newImage;
@@ -113,21 +138,21 @@ export const updateVehicleType = async (req, res) => {
 
     await vehicleType.save();
     return res.status(200).json({
-      message: responses.messages[language ].vehicleTypeUpdatedSuccessfully,
+      message: responses.messages[language].vehicleTypeUpdatedSuccessfully,
       result: vehicleType
     });
   } catch (error) {
-    return res.status(500).json({ error: responses.errors[language ].internalServerError });
+    return res.status(500).json({ error: responses.errors[language].internalServerError });
   }
 };
 
 export const deleteVehicleType = async (req, res) => {
-  const language = getLanguage(req,responses);
+  const language = getLanguage(req, responses);
 
   try {
     const vehicleType = await VehicleType.findById(req.params.id);
     if (!vehicleType) {
-      return res.status(404).json({ error: responses.errors[language ].vehicleTypeNotFound });
+      return res.status(404).json({ error: responses.errors[language].vehicleTypeNotFound });
     }
 
     if (vehicleType.image) {
@@ -140,8 +165,8 @@ export const deleteVehicleType = async (req, res) => {
     }
 
     await VehicleType.findByIdAndDelete(req.params.id);
-    return res.status(200).json({ message: responses.messages[language ].vehicleTypeDeletedSuccessfully });
+    return res.status(200).json({ message: responses.messages[language].vehicleTypeDeletedSuccessfully });
   } catch (error) {
-    return res.status(500).json({ error: responses.errors[language ].internalServerError });
+    return res.status(500).json({ error: responses.errors[language].internalServerError });
   }
 };
--- models/vehicleType.model.js
+++ models/vehicleType.model.js
@@ -3,6 +3,7 @@ import { Schema, model } from "mongoose";
 const vehicleTypeSchema = new Schema({
   name: { type: String, required: true },
   image: { type: String, required: true },
+  gstPercentage: { type: Number, required: true },
   hourlyPrices: [{ hour: Number, price: Number }],
 }, { timestamps: true, versionKey: false });
 
